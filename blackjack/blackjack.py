674c7c16e827a2a3ee4c09eff5ca06c40bf28afe
Freedom <froggiedevelopment@gmail.com>
2018-04-02 16:19:59 -0700

    working on blackjack game...

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -2,12 +2,12 @@ import card
 import deck
 import hand
 import player
+import advise
 
 
 def get_number_of_decks():
     num_decks = int(input('How many decks will we play?: '))
     deck_obj = deck.Deck(num_decks)
-
     return deck_obj
 
 
@@ -34,6 +34,12 @@ def win_normal(player_obj):
     win_amount = player_obj.bet + player_obj.bet
     player_obj.bet = 0
     player_obj.money += win_amount
+    return True
+
+
+def dealer_lose(lst_of_players):
+    for player_obj in lst_of_players:
+        win_normal(player_obj)
     return True
 
 
@@ -72,7 +78,7 @@ def deal_all_bets(lst_of_players):
 
 
 def deal_single_card(player_obj, deck_obj):
-    player_obj.hand_of_cards.insert(deck_obj.deal_card())
+    player_obj.hand_of_cards.get_card(deck_obj.deal_card())
     return True
 
 
@@ -99,101 +105,11 @@ def reveal_player_cards(player_obj):
     return reveal
 
 
-def pairs(player_hand_value, dealer_hand_cards):
-    dealer_shown_value = dealer_hand_cards[0].bk_value
-    if player_hand_value == 2 or player_hand_value == 16:
-        result = 'SP'
-    if player_hand_value == 20:
-        result = 'S'
-    if player_hand_value == 18:
-        result = 'SP'
-        if dealer_shown_value == 7 or dealer_shown_value == 10 or dealer_shown_value == 1:
-            result = 'S'
-    if player_hand_value == 14:
-        result = 'SP'
-        if dealer_shown_value > 7:
-            result = 'H'
-    if player_hand_value == 10:
-        result = 'Dh'
-        if dealer_shown_value > 9:
-            result = 'H'
-    if player_hand_value == 8:
-        result = 'H'
-        if dealer_shown_value == 5 or dealer_shown_value == 6:
-            result = 'SP'
-    if player_hand_value == 4 or player_hand_value == 6:
-        result = 'SP'
-        if dealer_shown_value > 7:
-            result = 'H'
+def has_bust(player_obj):
+    result = False
+    if player_obj.hand_of_cards.hand_value > 21:
+        result = True
     return result
-
-
-def soft_totals(player_hand_value, dealer_hand_cards):
-    dealer_shown_value = dealer_hand_cards[0].bk_value
-    if player_hand_value > 8:
-        result = 'S'
-        if dealer_shown_value == 6:
-            result = 'Ds'
-    if player_hand_value == 8:
-        result = 'Ds'
-        if dealer_shown_value == 7 or dealer_shown_value == 8:
-            result = 'S'
-        if dealer_shown_value > 8:
-            result = 'H'
-    if player_hand_value == 7:
-        result = 'H'
-        if 2 < dealer_shown_value < 7:
-            result = 'Dh'
-    if player_hand_value == 5 or player_hand_value == 6:
-        result = 'H'
-        if 3 < dealer_shown_value < 7:
-            result = 'Dh'
-    if player_hand_value == 3 or player_hand_value == 4:
-        result = 'H'
-        if 4 < dealer_shown_value < 7:
-            result = 'Dh'
-    return result
-
-
-def hard_totals(player_hand_value, dealer_hand_cards):
-    dealer_shown_value = dealer_hand_cards[0].bk_value
-    if player_hand_value > 16:
-        result = 'S'
-    elif player_hand_value > 12:
-        result = 'S'
-        if dealer_shown_value > 6:
-            result = 'H'
-    if player_hand_value == 12:
-        result = 'H'
-        if 3 < dealer_shown_value < 7:
-            result = 'S'
-    if player_hand_value == 11:
-        result = 'Dh'
-    if player_hand_value == 10:
-        result = 'Dh'
-        if dealer_shown_value > 9:
-            result = 'H'
-    if player_hand_value == 9:
-        result = 'H'
-        if 2 < dealer_shown_value < 7:
-            result = 'Dh'
-    if player_hand_value < 9:
-        result = 'H'
-    return result
-
-
-def advise(player_obj, dealer_obj):
-    for ind_card in player_obj.hand_of_cards.play_cards:
-        if len(player_obj.hand_of_cards.play_cards) == 2 and player_obj.hand_of_cards.play_cards[0] == player_obj.hand_of_cards.play_cards[1]:
-            result = pairs(player_obj.hand_of_cards.hand_value, dealer_obj.hand_of_cards.hand_of_cards)
-        elif ind_card.bk_value == 1 and player_obj.hand_of_cards.hand_value < 11:
-            result = soft_totals(player_obj.hand_of_cards.hand_value, dealer_obj.hand_of_cards.play_cards)
-        else: # player_obj.hand_of_cards.hand_value > 11:
-            result = hard_totals(player_obj.hand_of_cards.hand_value, dealer_obj.hand_of_cards.play_cards)
-    return result
-
-
-
 
 
 def has_split(player_obj):
@@ -202,6 +118,31 @@ def has_split(player_obj):
     if len(player_cards) == 2:
         if len(player_cards) != len(set(player_cards)):
             result = True
+    return result
+
+
+def has_blackjack(player_obj):
+    result = False
+    player_cards = player_obj.hand_of_cards.play_cards
+    if len(player_cards) == 2 and player_obj.hand_of_cards.hand_value == 11:
+        if player_cards[0].bk_value == 1 or player_cards[1].bk_value == 1:
+            result = True
+    return result
+
+
+def check_blackjack(player_obj):
+    result = False
+    if has_blackjack(player_obj):
+        print('BLACKJACK!')
+        win_blackjack(player_obj)
+        result = True
+    return result
+
+def check_dealer_blackjack(player_obj):
+    result = False
+    if has_blackjack(player_obj):
+        print('Dealer blackjack')
+        result = True
     return result
 
 
@@ -215,26 +156,104 @@ def get_player_choice(player_obj):
         print('(S)tand, (H)it, (D)ouble')
         poss_choice = ['S', 'H', 'D']
     while player_choice not in poss_choice:
-        player_choice = input('{}, what would you like to do'.format(player_obj.name)).capitalize()
+        player_choice = input('{}, what would you like to do: '.format(player_obj.name)).capitalize()
     return player_choice
 
 
-def play_players(lst_of_players):
+def play_players(lst_of_players, curr_deck):
     dealer_card = reveal_player_cards(lst_of_players[-1])
     for player_obj in lst_of_players[:-1]:
         player_hand = reveal_player_cards(player_obj)
-        advise_str = advise(player_obj, lst_of_players[-1])
+        check_blackjack(player_obj)
+        advise_str = advise.advise(player_obj, lst_of_players[-1])
+        #make better
         print(player_hand + dealer_card + advise_str)
-        play_choice = get_player_choice(player_obj)
-        print(play_choice)
+        while True:
+            play_choice = get_player_choice(player_obj)
+            if play_choice == 'S':
+                break
+            if play_choice == 'H':
+                deal_single_card(player_obj, curr_deck)
+                print(reveal_player_cards(player_obj))
+                bust = has_bust(player_obj)
+                if bust:
+                    lose(player_obj)
+                    play_choice = 'SU'
+                    break
+            if play_choice == 'D':
+                player_obj.money -= player_obj.bet
+                player_obj.bet += player_obj.bet
+                deal_single_card(player_obj, curr_deck)
+                print(reveal_player_cards(player_obj))
+                bust = has_bust(player_obj)
+                if bust:
+                    lose(player_obj)
+                    play_choice = 'SU'
+                    break
+    return True
+
+def play_dealer(lst_of_players, curr_deck):
+    dealer_hand = reveal_player_cards(lst_of_players[-1])
+    dealer_obj = lst_of_players[-1]
+
+    while dealer_obj.hand_of_cards.hand_value <= 16:
+        deal_single_card(lst_of_players[-1], curr_deck)
+        for a_card in dealer_obj.hand_of_cards.play_cards:
+            if a_card.bk_value == 1 and dealer_obj.hand_of_cards.hand_value < 11:
+                a_card.bk_value += 10
+    if dealer_obj.hand_of_cards.hand_value > 22:
+        for a_card in dealer_obj.hand_of_cards.play_cards:
+            if a_card.bk_value == 11:
+                a_card.bk_value = 1
+                break
+            else:
+                dealer_lose(lst_of_players)
+                print('dealer lost')
+                break
+    for player_obj in lst_of_players:
+        if player_obj.hand_of_cards.hand_value == dealer_obj.hand_of_cards.hand_value:
+            player_obj.money += player_obj.bet
+            if player_obj.name == 'Dealer':
+                pass
+            else:
+                print('dealer pushed with {}'.format(player_obj.name))
+        elif player_obj.hand_of_cards.hand_value > dealer_obj.hand_of_cards.hand_value and player_obj.hand_of_cards.hand_value < 22:
+            player_obj.money += player_obj.bet * 2
+            print('dealer lost to {}'.format(player_obj.name))
+        else:
+            player_obj.bet = 0
+    return True
+
+def reset_hands(lst_of_players):
+    for player_obj in lst_of_players:
+        player_obj.hand_of_cards = hand.Hand()
+    return True
 
 
 def main():
     lst_parti = create_players()
     hopper = get_number_of_decks()
-    deal_all_bets(lst_parti)
-    deal_first_two_cards(lst_parti, hopper)
-    play_players(lst_parti)
+    while True:
+        reset_hands(lst_parti)
+        deal_all_bets(lst_parti)
+        for index, player in enumerate(lst_parti):
+            if player.bet == 0 and player.money == 0:
+                remove_player(lst_parti, index)
+                print('{} has been removed'.format(player.name))
+        deal_first_two_cards(lst_parti, hopper)
+        if check_dealer_blackjack(lst_parti[-1]):
+            for player in lst_parti:
+                if check_blackjack(player):
+                    player.money += player.bet
+                    lose(player)
+                else:
+                    lose(player)
+
+            continue
+        print('playing players')
+        play_players(lst_parti, hopper)
+        print('playing dealer')
+        play_dealer(lst_parti, hopper)
 
 
 if __name__ == '__main__':
